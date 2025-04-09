25f6b3975835d47099ea38da85e4fa274cf82af0
Gwen Lg <me@gwenlg.fr>
2025-04-10 01:08:15 +0200

    feat: add pixel_pass_through fn to avoid to have to manual implement it

--- src/pgs/mod.rs
+++ src/pgs/mod.rs
@@ -12,7 +12,7 @@ mod sup;
 mod u24;
 
 pub use decoder::{DecodeTimeImage, DecodeTimeOnly, PgsDecoder};
-pub use pgs_image::{RleEncodedImage, RleToImage};
+pub use pgs_image::{pixel_pass_through, RleEncodedImage, RleToImage};
 pub use segment::{SegmentBuf, SegmentTypeCode};
 pub use sup::SupParser;
 
